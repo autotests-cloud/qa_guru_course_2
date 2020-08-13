68b54278853fba9a9ef6dab08835847f20b80f9e
svasenkov <aanher@gmail.com>
2020-08-13 19:59:59 +0300

    return commit

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -6,17 +6,21 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
-class GoogleTest {
+
+class GoogleTests {
+
     @Test
     void selenideSearchTest() {
         // Открыть google
         open("https://google.com");
 
         // Ввести Selenide в поиск
-        $(byName("q")).setValue("Selenide").pressEnter();
+        $(byName("q")).setValue("Selenide").pressEnter(); // кнопка логина
 
         // Проверить, что Selenide появился в результатах поиска
         $("html").shouldHave(text("ru.selenide.org"));
     }
+
 }
