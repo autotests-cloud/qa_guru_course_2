967dddfed3f1b35c8a6c78f2ae744d4df0217d7b
svasenkov <aanher@gmail.com>
2020-08-13 21:23:42 +0300

    added test with executeJavascript

---
+++ src/test/java/tests/GoogleWithExecuteScriptTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+
+class GoogleWithExecuteScriptTests {
+
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).shouldBe(visible);
+        executeJavaScript("$(\"[name='q']\").setAttribute(\"value\", \"123\")");
+        $(byName("q")).pressEnter(); // кнопка логина
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(text("ru.selenide.org"));
+    }
+
+}
