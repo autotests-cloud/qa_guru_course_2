7fb1c30bb9995af00300a938b8a17973684885d2
Vasenkov Stanislav <aanher@gmail.com>
2020-09-28 21:42:32 +0300

    Update GoogleTests.java
    restored repository

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -1,21 +1,16 @@
 package tests;
 
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-
 
 class GoogleTests {
 
     @Test
     void selenideSearchTest() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-
         // Открыть google
         open("https://google.com");
 
